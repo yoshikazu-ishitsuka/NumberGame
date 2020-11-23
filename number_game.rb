8de1579868271d5465d8c032f15f66f1708b8209
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-11-23 17:44:52 +0900

    Refactoring

--- number_game.rb
+++ number_game.rb
@@ -38,18 +38,15 @@ def input_hand(name)
   hand
 end
 
-# コールターン
-def call_turn(name, turn, hand_array, list)
-  puts "プレイヤー#{name}さんの第#{turn}ターンです。\n\n"
+# コール履歴の出力
+def call_history(list)
+  puts "###### これまでのコール履歴 ########"
+  list.each { |hash| puts "# 第#{hash[:turn]}ターン：#{hash[:call]} => #{hash[:judgement]} #" }
+  puts "####################################\n\n"
+end
 
-  if list.length > 0
-    puts "###### これまでのコール履歴 ########"
-    list.each do |hash|
-      puts "# 第#{hash[:turn]}ターン：#{hash[:call]} => #{hash[:judgement]} #"
-    end
-    puts "####################################\n\n"
-  end
-
+# コールする数字を入力する
+def input_call
   print "コールする数字を入力してください。\n入力受付中 => "
   call = gets.chomp
   puts "\n"
@@ -60,7 +57,39 @@ def call_turn(name, turn, hand_array, list)
     puts "\n"
   end
 
-  print "『#{call}ですね。 結果は』"
+  call
+end
+
+# EAT-BITE判定
+def judge_eat_bite(call_array, hand_array)
+  eat = bite = 0
+
+  3.times do |i|
+    3.times do |j|
+      if call_array[i] == hand_array[j] && i == j
+        eat += 1
+      elsif call_array[i] == hand_array[j]
+        bite += 1
+      end
+    end
+  end
+
+  eat == 3 ? judgement = "[3]EAT! 見事相手の手札を見破りました!" : judgement = "[#{eat}]EAT-[#{bite}]BITE"
+
+  puts "#{judgement}\n\n\n"
+
+  judgement
+end
+
+# コールターン
+def call_turn(name, turn, hand_array, list)
+  puts "プレイヤー#{name}さんの第#{turn}ターンです。\n\n"
+
+  call_history(list) if list.length > 0
+
+  call = input_call
+
+  print "『#{call}』ですね。 結果は"
   5.times do
     sleep(0.3)
     print "."
@@ -68,25 +97,8 @@ def call_turn(name, turn, hand_array, list)
   puts "\n\n"
 
   call_array = call.split("")
-  eat = bite = 0
+  judgement = judge_eat_bite(call_array, hand_array)
 
-  3.times do |i|
-    3.times do |j|
-      if call_array[i] == hand_array[j] && i == j
-        eat = eat + 1
-      elsif call_array[i] == hand_array[j]
-        bite = bite + 1
-      end
-    end
-  end
-
-  if eat == 3
-    judgement = "[3]EAT! 見事相手の手札を見破りました!"
-  else
-    judgement = "[#{eat}]EAT-[#{bite}]BITE"
-  end
-
-  puts "#{judgement}\n\n\n"
   sleep(0.5)
   puts "----------- ターン終了 ---------------\n\n\n\n"
   sleep(0.5)
@@ -97,9 +109,7 @@ end
 # 3EATが含まれているか判定する
 def three_eat?(list)
   three_eat = "[3]EAT! 見事相手の手札を見破りました!"
-  list.any? do |hash|
-    hash.value?(three_eat)
-  end
+  list.any? { |hash| hash.value?(three_eat) }
 end
 
 ### ここから出力開始 ###
@@ -121,20 +131,20 @@ while true
   b_call = call_turn("B", turn, a_hand_array, b_called_list)
   b_called_list.push(b_call)
 
-  if three_eat?(a_called_list) || three_eat?(b_called_list)
-    break
+  break if three_eat?(a_called_list) || three_eat?(b_called_list)
+
+  turn += 1
+end
+
+result = if three_eat?(a_called_list) && three_eat?(b_called_list)
+    "引き分け"
+  elsif three_eat?(a_called_list)
+    "プレイヤーAさんの勝ち!"
+  elsif three_eat?(b_called_list)
+    "プレイヤーBさんの勝ち!"
   end
 
-  turn = turn + 1
-end
-
-if three_eat?(a_called_list) && three_eat?(b_called_list)
-  puts "ゲーム終了！ 最終結果は『引き分け』です！\n\n"
-elsif three_eat?(a_called_list)
-  puts "ゲーム終了！ 最終結果は『プレイヤーAさんの勝ち』です！\n\n"
-elsif three_eat?(b_called_list)
-  puts "ゲーム終了！ 最終結果は『プレイヤーBさんの勝ち』です！\n\n"
-end
+puts "ゲーム終了！ 最終結果は『#{result}』です！\n\n"
 
 # 1.プレイヤーAが0~9の10枚のカードの中から手札を決める
 # 2.プレイヤーBが0~9の10枚のカードの中から手札を決める
